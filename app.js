de0dd4a80a55d21935aea947d92a203f91cbe72b
Ashar Ashfaq <ashar@caary.com>
2022-11-01 01:31:16 +0500

    adding chat to game.

--- app.js
+++ app.js
@@ -110,6 +110,40 @@ Player.update = function () {
     return pack
 }
 
+var Bullet = function (angle) {
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle / 180 * Math.PI) * 10;
+    self.spdY = Math.cos(angle / 180 * Math.PI) * 10;
+    self.timer = 0;
+    self.toRemove = false;
+    var super_update = self.update;
+    self.update = function () {
+        if (self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+    }
+    Bullet.list[self.id] = self;
+    return self;
+}
+Bullet.list = {}
+
+Bullet.update = function () {
+    if (Math.random() < 0.1) {
+        Bullet(Math.random() * 360)
+    }
+    var pack = [];
+    for (var i in Bullet.list) {
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x: bullet.x,
+            y: bullet.y,
+        })
+    }
+    return pack;
+}
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function (socket) {
     try {
@@ -121,6 +155,13 @@ io.sockets.on('connection', function (socket) {
             Player.onDisconnect(socket);
         })
 
+        socket.on('sendMsgToServer', function (data) {
+            var playerName = ("" + socket.id).slice(2, 7);
+            for(var i in SOCKET_LIST){
+                SOCKET_LIST[i].emit('addToChat', playerName + ": " + data);
+            }
+        })
+
 
     } catch (e) {
         console.log("error", e.message);
@@ -130,13 +171,16 @@ io.sockets.on('connection', function (socket) {
 
 setInterval(function () {
     try {
-        var pack = Player.update()
+        var pack = {
+            player: Player.update(),
+            bullet: Bullet.update()
+        }
         for (var i in SOCKET_LIST) {
             var socket = SOCKET_LIST[i];
             socket.emit('newPositions', pack)
         }
     } catch (e) {
-        console.log("ye wala",e.message);
+        console.log("ye wala", e.message);
     }
 
 }, 1000 / 25)
